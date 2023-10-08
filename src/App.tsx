3c125a334fed96706a8657ba165d52bf1d32a4bb
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-08 23:01:02 +0200

    update taskfy directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Playground from "./components/Playground/Playground"
-
+import Taskfy from "./components/Taskify/Taskfy"
 function App() {
 
   return (
     <>
         <Playground/>
-        
+        <Taskfy/>
     </>
   )
 }
--- src/components/Taskify/Taskfy.tsx
+++ src/components/Taskify/Taskfy.tsx
@@ -1,8 +1,11 @@
+import InputField from "./InputField";
+
 export default function Taskfy() {
   return (
     <>
       <section className="w-full h-full flex flex-col items-center bg-blue-600">
         <span className="text-3xl uppercase mt-10 text-white">Taskfy</span>
+        <InputField/>
       </section>
     </>
   );
