cf8e0defbec8857f4d9bd3aec413e3ab37b7ec89
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-09 07:34:16 +0200

    feat: add the taskfy state and  props

--- src/components/Taskify/InputField.tsx
+++ src/components/Taskify/InputField.tsx
@@ -1,13 +1,23 @@
-export default function InputField() {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function InputField({todo, setTodo}:Props) {
   return (
     <>
       <form className="flex w-4/5 relative items-center">
         <input 
             type="text" 
             placeholder="Enter a task"
-            className="w-full rounded-2xl py-5 px-6 text-2xl border-none transition-all delay-75 shadow-md"
+            className="w-full rounded-2xl py-5 px-6 text-2xl border-none transition-all delay-75 shadow-md mt-3 outline-none"
+            value={todo}
+            onChange={
+              (e) => setTodo(e.target.value) 
+            }
             />
-        <button className="p-4 m-3  rounded-xl absolute right-0 border-none text-lg bg-blue-600 text-white shadow-lg cursor-pointer outline-none">Add</button>
+        <button 
+            className="p-4 m-3  rounded-xl absolute right-0 border-none text-lg bg-blue-600 text-white shadow-lg cursor-pointer">Add</button>
       </form>
     </>
   )
--- src/components/Taskify/Taskfy.tsx
+++ src/components/Taskify/Taskfy.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import InputField from "./InputField";
 
 export default function Taskfy() {
+  const [todo, setTodo] = useState<string>("")
   return (
     <>
       <section className="w-full h-full flex flex-col items-center bg-blue-400">
         <span className="text-3xl uppercase mt-10 text-white">Taskfy</span>
-        <InputField/>
+        <InputField todo={todo} setTodo={setTodo}/>
       </section>
     </>
   );
