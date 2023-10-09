98c2a706400d1a23c93036b178629a3dfa864b09
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-09 07:07:30 +0200

    style: update button styles

--- src/components/Taskify/InputField.tsx
+++ src/components/Taskify/InputField.tsx
@@ -7,7 +7,7 @@ export default function InputField() {
             placeholder="Enter a task"
             className="w-full rounded-2xl py-5 px-6 text-2xl border-none transition-all delay-75 shadow-md"
             />
-        <button className="p-4 m-3  rounded-xl absolute right-0 border-none text-lg bg-blue-600 text-white shadow-lg">Add</button>
+        <button className="p-4 m-3  rounded-xl absolute right-0 border-none text-lg bg-blue-600 text-white shadow-lg cursor-pointer outline-none">Add</button>
       </form>
     </>
   )
--- src/components/Taskify/Taskfy.tsx
+++ src/components/Taskify/Taskfy.tsx
@@ -3,7 +3,7 @@ import InputField from "./InputField";
 export default function Taskfy() {
   return (
     <>
-      <section className="w-full h-full flex flex-col items-center bg-blue-600">
+      <section className="w-full h-full flex flex-col items-center bg-blue-400">
         <span className="text-3xl uppercase mt-10 text-white">Taskfy</span>
         <InputField/>
       </section>
