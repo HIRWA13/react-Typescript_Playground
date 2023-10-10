b544907b79dae194c9f117c4031edbd55ec1713b
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-10 08:15:47 +0200

    docs: comment the Playground and Taskfy components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Playground from "./components/Playground/Playground"
-import Taskfy from "./components/Taskify/Taskfy"
+// import Playground from "./components/Playground/Playground"
+// import Taskfy from "./components/Taskify/Taskfy"
+import Greet from "./components/CrashCourse/Greet"
 function App() {
 
   return (
     <>
-        <Playground/>
-        <Taskfy/>
+        {/* <Playground/>
+        <Taskfy/> */}
+       <Greet name="Junior"/>
     </>
   )
 }
